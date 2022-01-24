25c30f52ea7143d1082873124fcbc686b4594722
D-Card <diogocardoso67@tecnico.ulisboa.pt>
2022-01-24 11:38:17 +0000

    Use 3 DFS

--- project.cpp
+++ project.cpp
@@ -1,15 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
+enum color {
+	white,
+	grey,
+	black,
+};
+
 typedef struct node {
-	bool visited1 = false; // check
-	bool visited2 = false; // search node's ancestors
-	int parent1 = -1;	   // index
+	int bloodline = -1; // search node's ancestors
+	int parent1 = -1;	// index
 	int parent2 = -1;
+	int color = white; // used for loop checking
 } node;
 
-bool build_tree(std::vector<node> &tree, int EDGE_NUMBER)
+bool build_tree(std::vector<node> &tree, int edge_number)
 {
 	int x, y, count = 0;
 	while (std::cin >> x) {
@@ -23,78 +30,79 @@ bool build_tree(std::vector<node> &tree, int EDGE_NUMBER)
 		}
 		count++;
 	}
-	if (count != EDGE_NUMBER)
+	if (count != edge_number)
 		return false; // needed to pass the professors' tests
 
 	return true;
 }
 
-//GIGA TODO: refactor everything
-bool DFS_visit_and_check_tree_and_find_closest_common_ancestors(std::vector<node> &tree, int v, int v1, int v2, bool visit2, std::vector<int> &common_ancestors)
+bool DFS_Visit_Complete(std::vector<node> &tree, int i)
 {
-	if (v == v1 || v == v2) {
-		// cenas
-	} else {
-		if (tree[v].visited1 == grey) {
-			// found cycle! :) XD
-			return false;
-		}
-		if (visit2 && tree[v].visited2) {
-			// common ancestor
-			common_ancestors.push_back(v);
-			return true;
-		}
-
-		tree[v].visited1 = true;
-		if (v == v1 || v == v2) {
-			tree[v].visited2 = true;
-		}
-
-		if (tree[v].parent1 != -1 && !tree[tree[v].parent1].visited1) {
-			if (!DFS_visit_and_check_tree_and_find_closest_common_ancestors(tree, tree[v].parent1)) {
-				return false;
-			}
-		}
-		if (tree[v].parent2 != -1 && !tree[tree[v].parent2].visited1) {
-			if (!DFS_visit_and_check_tree_and_find_closest_common_ancestors(tree, tree[v].parent2)) {
-				return false;
-			}
-		}
-		// tree[v].visited1 = black;
+	if (tree[i].color == grey) {
+		return false;
 	}
+	if (tree[i].color == white) {
+		tree[i].color = grey;
+		if (tree[i].parent1 != -1) {
+			DFS_Visit_Complete(tree, tree[i].parent1);
+		}
+		if (tree[i].parent2 != -1) {
+			DFS_Visit_Complete(tree, tree[i].parent2);
+		}
+		tree[i].color = black;
+	}
+	return true;
 }
 
-bool name_me(std::vector<node> &tree, int v1, int v2, std::vector<int> &common_ancestors, int EDGE_NUMBER)
+//checks for loops
+bool DFS_Complete(std::vector<node> &tree, int tree_size)
 {
-	if (!build_tree(tree, EDGE_NUMBER))
-		return false;
-
-	for (size_t i = 1; i < tree.size(); i++)
-		if (!tree[i].visited1)
-			if (!DFS_visit_and_check_tree_and_find_closest_common_ancestors(tree, i, v1, v2, false, common_ancestors))
+	for (int i = 0; i < tree_size; i++) {
+		if (tree[i].color == white) {
+			if (!DFS_Visit_Complete(tree, i)) {
 				return false;
-
+			}
+		}
+	}
 	return true;
+}
+
+//TODO: Fix this function
+void DFS_Bloodline(std::vector<node> &tree, int i, int v1, int v2, std::vector<int> &common_ancestors)
+{
+
+	if (tree[i].bloodline == v2) {
+		common_ancestors.push_back(i);
+		std::cout << "Adding " << i << " to common ancestors" << std::endl;
+	} else {
+		tree[i].bloodline = v1;
+		if (tree[i].parent1 != -1) {
+			DFS_Bloodline(tree, tree[i].parent1, v1, v2, common_ancestors);
+		}
+		if (tree[i].parent2 != -1) {
+			DFS_Bloodline(tree, tree[i].parent2, v1, v2, common_ancestors);
+		}
+	}
 }
 
 int main()
 {
-	int i;
-	int v1, v2, TREE_SIZE, EDGE_NUMBER;
-	std::cin >> v1 >> v2 >> TREE_SIZE >> EDGE_NUMBER;
-	std::vector<node> tree(TREE_SIZE + 1); // 0 isn't used, it still works tho :sunglasses:
+	int v1, v2, tree_size, edge_number;
+	std::cin >> v1 >> v2 >> tree_size >> edge_number;
+	std::vector<node> tree(tree_size + 1); // 0 isn't used, it still works tho :sunglasses:
 	std::vector<int> common_ancestors;
-	if (name_me(tree, v1, v2, common_ancestors, EDGE_NUMBER)) {
-		if (common_ancestors.size() == 0) {
-			std::cout << "-" << std::endl;
-		} else {
-			for (int i : common_ancestors) {
-				std::cout << i << " ";
-			}
-			std::cout << std::endl;
-		}
-	} else {
-		std::cout << "0" << std::endl;
+	if (!build_tree(tree, edge_number) || !DFS_Complete(tree, tree_size)) {
+		std::cout << "-" << std::endl;
+		return 0;
+	}
+	std::cout << "Building tree complete" << std::endl;
+	DFS_Bloodline(tree, v1, v1, v2, common_ancestors);
+	std::cout << "First Bloodline complete" << std::endl;
+	DFS_Bloodline(tree, v2, v2, v1, common_ancestors);
+	std::cout << "Second Bloodline complete" << std::endl;
+	sort(common_ancestors.begin(), common_ancestors.end());
+	for (size_t i = 0; i < common_ancestors.size(); i++) {
+		std::cout << common_ancestors[i] << " ";
 	}
 	return 0;
 }
