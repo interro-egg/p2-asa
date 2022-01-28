ab2f72e1fc9617cb941660fefaa93a4000e548ce
Rafael Oliveira <56204853+RafDevX@users.noreply.github.com>
2022-01-28 16:21:52 +0000

    Data Analysis & Graph (#5)

---
+++ analysis/run.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# you probably want to call this with `time ./run.sh [custom_results_csv] [custom_exe] && date`
+
+cd "${0%/*}" # go to script directory
+set -e # exit if any command fails
+
+CSV="${1:-$(date +%s).csv}" # arg 1 with default value
+EXE="${2:-../project}" # arg 2 with default value
+
+
+if [ ! -f "$EXE" ]; then
+	echo "Executable (arg #2, defaults to ../project) does not exist!"
+	echo -e "\tFile '$EXE' not found"
+	exit 1
+fi
+
+# This uses the randGeneoTree program provided by professors to generate trees. The #V parameter is gradually increased, #p is always $EDGE_PROB and no seed is provided.
+EDGE_PROB=0.99
+
+g++ -std=c++11 -O3 -Wall randGeneoTree.cpp -lm -o randGeneoTree # there's no need to always compile it, but it's easier this way
+
+
+mkdir -p gend_trees
+
+echo "V,E,command,mean,stddev,median,user,system,min,max" > $CSV
+
+test() {
+	echo "Testing with V = $1"
+	./randGeneoTree $1 $EDGE_PROB > in.txt
+	EDGES=$(head in.txt -n2 | tail -n1 | cut -d' ' -f2)
+	cp in.txt gend_trees/in_$1.txt
+	hyperfine "$EXE < in.txt" --warmup 10 --export-csv tmp.csv &>/dev/null
+	echo -n $1,$EDGES, >> $CSV
+	grep ./project tmp.csv >> $CSV # remove header
+	rm in.txt
+	rm tmp.csv
+}
+
+#for i in {10..99..1}; do
+#	test $i
+#done
+
+#for i in {100..999..10}; do
+#	test $i
+#done
+
+#for i in {1000..9999..100}; do
+#	test $i
+#done
+
+#for i in {10000..99999..1000}; do
+#	test $i
+#done
+
+for i in {100000..1000000..10000}; do
+    test $i
+done
+
+#for i in {1000000..10000000..100000}; do
+#    test $i
+#done
